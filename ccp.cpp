d3ea779e33fbcd741a578000c8e0f21624761d02
Samyog Shrestha <184324726+samiiigo@users.noreply.github.com>
2025-01-29 13:04:00 -0600

    lambda

--- ccp.cpp
+++ ccp.cpp
@@ -2,9 +2,11 @@
 
 int main() {
     std::string x;
-    std::cin>>x;
+    std::cin>>x;;
 
-    std::cout << x << " says " << "Hello, " << x << "!" << std::endl;
+    auto add = [](int a, int b) -> int {return a + b;};
+
+    std::cout << x << " says " << "Hello, " << add(x,x) << "!" << std::endl;
     std::cout << x << " says " << "Hello, " << x << "!" << std::endl;
     
 
